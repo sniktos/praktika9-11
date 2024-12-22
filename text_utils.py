62e019fa23e3616dc1b4fb37434c20b6a1887b6e
cntaf <gsdgs@fue.re>
2024-12-22 22:18:46 +0700

    Итерация 1: Функция генерации случайного текста

---
+++ text_utils.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+def generate_random_text(word_count):
+    """
+    Генерирует случайный текст из заданного количества слов.
+    """
+    vowels = "aeiou"
+    consonants = "bcdfghjklmnpqrstvwxyz"
+    words = []
+
+    for _ in range(word_count):
+        word_length = random.randint(3, 8)  # Длина слова от 3 до 8 символов
+        word = ''.join(random.choices(vowels + consonants, k=word_length))
+        words.append(word)
+
+    return ' '.join(words)
+
+# Тестирование
+if __name__ == "__main__":
+    random_text = generate_random_text(10)
+    print("Случайный текст:", random_text)
